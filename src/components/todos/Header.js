89099eb828dc286097e914a42ed28d7017f919ed
Hamza Mosaad <mezo2542002@gmail.com>
2023-12-14 23:02:41 +0200

    first commit

---
+++ src/components/todos/Header.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Header = () => {
+  const [TaskNum, setTaskNum] = useState(
+    localStorage.getItem("Tasks")
+      ? JSON.parse(localStorage.getItem("Tasks")).length
+      : 0
+  );
+  return (
+    <header>
+      <h1> قائمة المهام ({TaskNum}) </h1>
+    </header>
+  );
+};
+
+export default Header;
